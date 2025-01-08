33ac8cf3b2da8de7a4cb777ba6ef04d6e2e1d601
MithuAhammad <m.h.mithuonline@gmail.com>
2025-01-08 10:47:10 +0600

    Display Categories To Ui And Understand The Mechanism

--- scripts/video.js
+++ scripts/video.js
@@ -6,12 +6,26 @@ const loadCategories = () => {
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
     .then((res) => res.json())
     .then((data) => displayCategories(data.categories))
-    .catch((err) => console.log(err))
+    .catch((err) => console.log(err));
 };
-
+/*
+ {
+      "category_id": "1001",
+      "category": "Music"
+    }
+*/
 // Create Display Categories
-const displayCategories = (data) => {
-  console.log(data)
+const displayCategories = (categories) => {
+  const categoryContainer = document.getElementById("categories");
+  categories.forEach((item) => {
+    console.log(item);
+    // Create a button
+    const button = document.createElement("button");
+    button.classList = "btn";
+    button.innerText = item.category;
+    // add button to category
+    categoryContainer.append(button);
+  });
 };
 
 loadCategories();
