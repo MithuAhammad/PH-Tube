b0b208b4eb450c9ebdc4f2bf5dd258b62def77d1
MithuAhammad <m.h.mithuonline@gmail.com>
2025-01-11 13:15:07 +0600

    Recap The Mechanism , Load Videos And Display Cards On The Ui

--- scripts/video.js
+++ scripts/video.js
@@ -8,6 +8,16 @@ const loadCategories = () => {
     .then((data) => displayCategories(data.categories))
     .catch((err) => console.log(err));
 };
+
+// Create video api patch
+const loadVideos = () => {
+  //   fetch data
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.videos))
+    .catch((err) => console.log(err));
+};
+
 /*
  {
       "category_id": "1001",
@@ -18,7 +28,7 @@ const loadCategories = () => {
 const displayCategories = (categories) => {
   const categoryContainer = document.getElementById("categories");
   categories.forEach((item) => {
-    console.log(item);
+    // console.log(item);
     // Create a button
     const button = document.createElement("button");
     button.classList = "btn";
@@ -27,5 +37,28 @@ const displayCategories = (categories) => {
     categoryContainer.append(button);
   });
 };
+const displayVideos = (videos) => {
+  const videoContainer = document.getElementById("videos");
+  videos.forEach((video) => {
+    const card = document.createElement("div");
+    card.classList = "card card-compact ";
+    card.innerHTML = `
+     <figure>
+    <img
+      src="${video.thumbnail}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p>${video.}</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+    `;
+    videoContainer.append(card);
+  });
+};
 
 loadCategories();
+loadVideos();
