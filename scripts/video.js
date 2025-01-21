793335e107c9f39a755a6e426e753528920a64fa
MithuAhammad <m.h.mithuonline@gmail.com>
2025-01-21 12:19:34 +0600

    35-9 Show Conditional No Content Text For Empty Array

--- scripts/video.js
+++ scripts/video.js
@@ -61,6 +61,19 @@ const displayCategories = (categories) => {
 const displayVideos = (videos) => {
   const videoContainer = document.getElementById("videos");
   videoContainer.innerHTML = "";
+  if (videos.length == 0) {
+    videoContainer.classList.remove("grid");
+    videoContainer.innerHTML = `
+    <div class="min-h-[300px]w-full flex flex-col gap-5 justify-center items-center">
+    <img src="assets/Icon.png" alt="" />
+    <h2 class="text-center text-xl font-bold">No Content Here I this Category</h2>
+    </div>
+    `;
+    return;
+  } else {
+    videoContainer.classList.add("grid");
+  }
+
   videos.forEach((video) => {
     const card = document.createElement("div");
     card.classList = "card card-compact";
